45ad21cc21e3348f3419156f00e48900f19dfd52
Parker Siu <parker.siu@gmail.com>
2023-12-28 14:33:17 -0800

    Added hero section with motion animations.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
       {/* About */}
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
       {/*Contact Me */}
-    </main>
+    </div>
   );
 }
