6d09d8b67dd5bac229a56075ede5a36043d14ffc
Parker Siu <parker.siu@gmail.com>
2023-12-29 13:32:57 -0800

    Added react hooks form and contact me section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import ContactMe from "@/components/ContactMe";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -7,7 +8,7 @@ import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Header />
       <section id="hero" className="snap-start">
         <Hero />
@@ -24,7 +25,9 @@ export default function Home() {
       <section id="projects" className="snap-start">
         <Projects />
       </section>
-      {/*Contact Me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 }
