6b84757dc96eaeffa6b5095267651bcc78e87924
Parker Siu <parker.siu@gmail.com>
2023-12-29 13:43:59 -0800

    Added tailwind scrollbar and custom scrollbars.

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header />
       <section id="hero" className="snap-start">
         <Hero />
@@ -28,6 +29,17 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="/Headshot.jpg"
+              alt="Headshot image"
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 }
