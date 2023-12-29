d28a6a41ca167dca6ebc1c372c72b61d4d4b6fd3
Parker Siu <parker.siu@gmail.com>
2023-12-29 12:01:54 -0800

    Added skills section and styling with placeholders.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
@@ -13,11 +14,12 @@ export default function Home() {
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experience */}
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
-      {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
       {/* Projects */}
       {/*Contact Me */}
     </div>
