9bf8f8aa0b1e37a86be7c21eaceb1bb5c94ca8d5
Parker Siu <parker.siu@gmail.com>
2024-01-05 15:46:17 -0800

    Moved footer iimage to bottom right on mobile.

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default async function Home() {
       </section>
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-end md:justify-center me-2 md:me-0">
             <img
               className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
               src="/Headshot.jpg"
