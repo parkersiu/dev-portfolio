d3219e9d0fdcd32dfe10fcb177befcd6b3d93310
Parker Siu <parker.siu@gmail.com>
2024-01-08 15:10:29 -0800

    Changed skills page to flex box for mobile views.

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -20,11 +20,11 @@ export default function Skills({ skills }: Props) {
         Skills
       </h3>
 
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+      <h3 className="absolute top-20 md:top-36 uppercase tracking-[3px] text-gray-500 text-sm">
         Hover over a skill for current experience
       </h3>
 
-      <div className="grid grid-cols-4 gap-5">
+      <div className="flex flex-4 flex-wrap justify-center md:grid md:grid-cols-4 gap-1 md:gap-5">
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
