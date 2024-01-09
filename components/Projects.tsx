4b60814a7106a3a2c7e73f8fb7beb7c60d18f03d
Parker Siu <parker.siu@gmail.com>
2024-01-08 16:06:54 -0800

    Improved styling of projects section.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,13 +14,13 @@ export default function Projects({ projects }: Props) {
       initial={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
-      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 pt-10"
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 pt-0 md:pt-10"
     >
       <h3 className="absolute top-8 md:top-20 uppercase tracking-widest md:tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
 
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="relative w-full flex overflow-x-scroll snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {projects?.map((project, i) => (
           <div
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
@@ -39,10 +39,7 @@ export default function Projects({ projects }: Props) {
             </a>
 
             <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-4xl font-semibold text-center">
-                <span className="underline decoration-[#F7AB0A]/50">
-                  Project {i + 1} of {projects.length}:
-                </span>{" "}
+              <h4 className="text-xl md:text-4xl font-semibold text-center">
                 {project?.title}
               </h4>
 
@@ -57,7 +54,7 @@ export default function Projects({ projects }: Props) {
                 ))}
               </div>
 
-              <p className="text-lg text-center md:text-left">
+              <p className="italic text-sm text-gray-300/70 md:text-lg md:text-left">
                 {project?.summary}
               </p>
             </div>
